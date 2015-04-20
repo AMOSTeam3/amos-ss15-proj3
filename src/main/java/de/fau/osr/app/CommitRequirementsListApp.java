d74d408ad619a6a3173585c5695f7100e7b43fdb
Tobias Polzer <tobias.polzer@fau.de>
2015-04-20 16:02:03 +0200

    Begin app for Req-4

---
+++ src/main/java/de/fau/osr/app/CommitRequirementsListApp.java
@@ -0,0 +1,14 @@
+package de.fau.osr.app;
+
+/**
+ * @author tobias
+ * This is an application to fulfill the acceptance criterion for Req-4
+ */
+public class CommitRequirementsListApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
