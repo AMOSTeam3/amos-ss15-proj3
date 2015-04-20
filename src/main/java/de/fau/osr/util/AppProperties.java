51210e32ee7344eaf71a7bda996862c93cacc504
gayathery <gayatherys@gmail.com>
2015-04-20 18:22:25 +0200

    Comments

--- src/main/java/de/fau/osr/util/AppProperties.java
+++ src/main/java/de/fau/osr/util/AppProperties.java
@@ -3,9 +3,18 @@ package de.fau.osr.util;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
-
+/**
+ * @author Gayathery
+ * @desc class to query property pages
+ *
+ */
 public class AppProperties {
 
+	/**
+	 * @param key
+	 * @desc This method returns the values of properties represented by the param
+	 * @author Gayathery
+	 */
 	public static String GetValue(String key)
 	{
 		Properties properties = new Properties();
