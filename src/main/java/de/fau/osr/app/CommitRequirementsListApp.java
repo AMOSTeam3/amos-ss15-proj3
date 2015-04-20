ad762702ed058e9126a8217fc406283b7daab560
Tobias Polzer <tobias.polzer@fau.de>
2015-04-20 16:47:27 +0200

    Fulfill Req-4
    Acceptance can be tested with
    mvn compile exec:java -Dexec.mainClass=de.fau.osr.app.CommitRequirementsListApp
    -Dexec.args='-repo *your repository*/.git'

--- src/main/java/de/fau/osr/app/CommitRequirementsListApp.java
+++ src/main/java/de/fau/osr/app/CommitRequirementsListApp.java
@@ -1,14 +1,45 @@
 package de.fau.osr.app;
 
+import java.util.Iterator;
+import java.util.List;
+
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+
+import de.fau.osr.core.vcs.base.VcsController;
+import de.fau.osr.core.vcs.base.VcsEnvironment;
+import de.fau.osr.parser.CommitMessageParser;
+import de.fau.osr.parser.GitCommitMessageParser;
+
+
 /**
  * @author tobias
  * This is an application to fulfill the acceptance criterion for Req-4
  */
 public class CommitRequirementsListApp {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	private static class CliOptions {
+		@Parameter(names = "-repo", required = true)
+		String repoURL;
+	}
 
+	public static void main(String[] args) {
+		CliOptions cli = new CliOptions();
+		new JCommander(cli, args);
+		final VcsController controller = new VcsController(VcsEnvironment.GIT);
+		CommitMessageParser parser = new GitCommitMessageParser();
+		controller.Connect(cli.repoURL);
+		for(String commitId : new Iterable<String>(){
+			@Override
+			public Iterator<String> iterator() {
+				return controller.getCommitList();
+			}}) {
+			String commitMsg = controller.getCommitMessage(commitId);
+			List<Integer> reqs = parser.parse(commitMsg);
+			for(Integer i : reqs) {
+				System.out.println("commit " + commitId + " references Req-" + i);
+			}
+		}
 	}
 
 }
