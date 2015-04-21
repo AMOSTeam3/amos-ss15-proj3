15c88216ed3d7067a3de08afee141cd6363bddb3
Tobias Polzer <tobias.polzer@fau.de>
2015-04-21 15:49:26 +0200

    Finished App for Req-5
    This should in theory fulfill Req-5, however there is a bug in
    GitVcsClient, where it tries to get the parent to a commit and it gets
    the right commit id, but getTree() returns null! I have no idea why this
    happens, it could be worked around by just instantiating a new RevWalk
    to get each parent commit.

--- src/main/java/de/fau/osr/app/CommitFileListingApp.java
+++ src/main/java/de/fau/osr/app/CommitFileListingApp.java
@@ -1,10 +1,40 @@
 package de.fau.osr.app;
 
+import java.util.Iterator;
+
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+
+import de.fau.osr.core.vcs.base.CommitFile;
+import de.fau.osr.core.vcs.base.VcsController;
+import de.fau.osr.core.vcs.base.VcsEnvironment;
+import de.fau.osr.parser.CommitMessageParser;
+import de.fau.osr.parser.GitCommitMessageParser;
+
 /**
  * @author tobias
  * This is an application to fulfill the acceptance criterion for Req-5
  */
 public class CommitFileListingApp {
+	private static class CliOptions {
+		@Parameter(names = "-repo", required = true)
+		String repoURL;
+		@Parameter(names = "-commit", required = true)
+		String commitId;
+	}
 	public static void main(String[] args) {
+		CliOptions cli = new CliOptions();
+		new JCommander(cli, args);
+		final VcsController controller = new VcsController(VcsEnvironment.GIT);
+		controller.Connect(cli.repoURL);
+		final String commitId = cli.commitId;
+		for(CommitFile file : new Iterable<CommitFile>() {
+
+			@Override
+			public Iterator<CommitFile> iterator() {
+				return controller.getCommitFiles(commitId);
+			}}) {
+			System.out.println(file.oldPath + " " + file.commitState + " " + file.newPath);
+		}
 	}
 }
