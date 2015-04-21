cc190f0888041ec9d5c58aceb859a6fb679c178b
Tobias Polzer <tobias.polzer@fau.de>
2015-04-21 15:28:12 +0200

    start work on fulfilling Req-5

---
+++ src/main/java/de/fau/osr/app/CommitFileListingApp.java
@@ -0,0 +1,10 @@
+package de.fau.osr.app;
+
+/**
+ * @author tobias
+ * This is an application to fulfill the acceptance criterion for Req-5
+ */
+public class CommitFileListingApp {
+	public static void main(String[] args) {
+	}
+}
