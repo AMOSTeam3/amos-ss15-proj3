fcc1c7d336e52ceaf43a07f6e246e7ec71c23602
Tobias Polzer <tobias.polzer@gmail.com>
2015-05-12 13:24:00 +0200

    clean up BlameTest

--- src/test/java/de/fau/osr/vcs/impl/GitBlameTest.java
+++ src/test/java/de/fau/osr/vcs/impl/GitBlameTest.java
@@ -2,11 +2,7 @@ package de.fau.osr.vcs.impl;
 
 import static org.junit.Assert.*;
 
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
@@ -15,9 +11,6 @@ import org.junit.Test;
 import com.google.common.collect.Lists;
 
 import de.fau.osr.PublicTestData;
-import de.fau.osr.bl.Tracker;
-import de.fau.osr.core.db.CSVFileDataSource;
-import de.fau.osr.core.db.DataSource;
 import de.fau.osr.core.vcs.interfaces.VcsClient.AnnotatedLine;
 import de.fau.osr.core.vcs.impl.GitVcsClient;
 import de.fau.osr.util.parser.CommitMessageParser;
@@ -31,11 +24,6 @@ public class GitBlameTest {
 	
 	@Test
 	public void getBlame() throws Exception {
-		Path repoFilePath = Paths.get(new File(PublicTestData.getGitTestRepo()).getAbsolutePath());
-		Path dataSrcFilePath= repoFilePath.getParent().resolve("dataSource.csv");
-		DataSource dataSource = new CSVFileDataSource(dataSrcFilePath.toFile());
-		Tracker tracker = new Tracker(client);
-		DataSource dataSrc = new CSVFileDataSource(dataSrcFilePath.toFile());
 		List<AnnotatedLine> blame = client.blame("TestFile4", new CommitMessageParser());
 		assertEquals(Collections.singletonList(new AnnotatedLine(Lists.<String>newArrayList("1"), "File 4")), Lists.newArrayList(blame));
 		blame = client.blame("LICENSE", new CommitMessageParser());
