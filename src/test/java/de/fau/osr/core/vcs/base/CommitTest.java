9724acdc969c1e37259357e4d8be709ffee3a8c0
kaobizz <kaobizz@gmail.com>
2015-05-18 23:55:14 +0200

    Test for commit, testing message initialization with unusual chars for id

--- src/test/java/de/fau/osr/core/vcs/base/CommitTest.java
+++ src/test/java/de/fau/osr/core/vcs/base/CommitTest.java
@@ -15,9 +15,19 @@ public class CommitTest {
         assertEquals("commit id should be equal the initialization id", "new_id", commitToTest.id);
     }
 
+    public void CommitConstructor_CommitIdInitializedWithNoUsualChars_allTheCharsReturnedProperly(){
+        String[] ids = {"   ", "$&/&$//", "09084§§§", " kl%45$$%$%", "\"\"__ __\"\"", "", null};
+        for (String id : ids){
+            Commit commitToTest = new Commit(id, null, null, null);
+            assertEquals("commit id should be equal the initialization id", id, commitToTest.id);
+        }
+    }
+
     @Test
     public void CommitConstructor_messageField_Test(){
         Commit commitToTest = new Commit(null, "msg ok", null, null);
         assertEquals("message should be equal the initialization message", "msg ok", commitToTest.message);
     }
+
+
 }
