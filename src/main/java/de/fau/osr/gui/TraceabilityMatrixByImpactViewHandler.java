5b9d1b170a831658695bc4334f40fbf601a06b8a
Dmitry Gorelenkov <dmitry.gorelenkov@gmx.net>
2015-05-20 09:15:17 +0200

    added ability to save generated matrix to CSV or PDF Req-28 Req-31

--- src/main/java/de/fau/osr/gui/TraceabilityMatrixByImpactViewHandler.java
+++ src/main/java/de/fau/osr/gui/TraceabilityMatrixByImpactViewHandler.java
@@ -1,24 +1,17 @@
 package de.fau.osr.gui;
 
-import java.awt.EventQueue;
-import java.awt.Font;
-
-import javax.swing.AbstractListModel;
-import javax.swing.GroupLayout;
-import javax.swing.GroupLayout.Alignment;
-import javax.swing.JFrame;
-import javax.swing.JList;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTable;
-import javax.swing.ListModel;
-import javax.swing.ScrollPaneConstants;
-import javax.swing.border.EmptyBorder;
-
 import de.fau.osr.bl.RequirementsTraceabilityMatrixByImpact;
 import de.fau.osr.gui.util.RequirementsTraceabilityByImpactTableModel;
-import javax.swing.JLabel;
+import de.fau.osr.util.matrix.MatrixTools;
+
+import javax.swing.*;
+import javax.swing.GroupLayout.Alignment;
 import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
 
 public class TraceabilityMatrixByImpactViewHandler extends JFrame {
 
@@ -48,9 +41,8 @@ public class TraceabilityMatrixByImpactViewHandler extends JFrame {
 	 * Create the frame.
 	 */
 	public TraceabilityMatrixByImpactViewHandler() {
-		this.requirementsTraceabilityMatrixByImpact = requirementsTraceabilityMatrixByImpact;
 		setTitle("Spice Traceability - Traceability Matrix by Impact");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		setBounds(100, 100, 715, 510);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -64,19 +56,42 @@ public class TraceabilityMatrixByImpactViewHandler extends JFrame {
 		
 		JLabel lblNewLabel = new JLabel("Files");
 		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 11));
+		
+		JButton btnToCsv = new JButton("To CSV");
+		btnToCsv.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				MatrixTools.SaveMatrixToCsv(requirementsTraceabilityMatrixByImpact, chooseFile("result.csv"));
+			}
+		});
+		
+		JButton btnToPdf = new JButton("To PDF");
+		btnToPdf.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				MatrixTools.SaveMatrixToPdf(requirementsTraceabilityMatrixByImpact, chooseFile("result.pdf"));
+			}
+		});
 		GroupLayout gl_contentPane = new GroupLayout(contentPane);
 		gl_contentPane.setHorizontalGroup(
-			gl_contentPane.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_contentPane.createSequentialGroup()
-					.addContainerGap()
-					.addComponent(lblNewLabel)
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 617, GroupLayout.PREFERRED_SIZE)
-					.addContainerGap(41, Short.MAX_VALUE))
-				.addGroup(Alignment.TRAILING, gl_contentPane.createSequentialGroup()
-					.addContainerGap(354, Short.MAX_VALUE)
-					.addComponent(lblRequirements)
-					.addGap(255))
+				gl_contentPane.createParallelGroup(Alignment.TRAILING)
+						.addGroup(Alignment.LEADING, gl_contentPane.createSequentialGroup()
+								.addContainerGap()
+								.addComponent(lblNewLabel)
+								.addPreferredGap(ComponentPlacement.RELATED)
+								.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+										.addGroup(gl_contentPane.createSequentialGroup()
+												.addComponent(btnToCsv)
+												.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+														.addGroup(gl_contentPane.createSequentialGroup()
+																.addPreferredGap(ComponentPlacement.RELATED, 226, Short.MAX_VALUE)
+																.addComponent(lblRequirements)
+																.addGap(255))
+														.addGroup(gl_contentPane.createSequentialGroup()
+																.addGap(18)
+																.addComponent(btnToPdf)
+																.addContainerGap())))
+										.addGroup(gl_contentPane.createSequentialGroup()
+												.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 617, GroupLayout.PREFERRED_SIZE)
+												.addContainerGap(33, Short.MAX_VALUE))))
 		);
 		gl_contentPane.setVerticalGroup(
 			gl_contentPane.createParallelGroup(Alignment.LEADING)
@@ -85,16 +100,19 @@ public class TraceabilityMatrixByImpactViewHandler extends JFrame {
 					.addPreferredGap(ComponentPlacement.RELATED)
 					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
 						.addGroup(gl_contentPane.createSequentialGroup()
-							.addGap(26)
+								.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+										.addComponent(btnToCsv)
+								.addComponent(btnToPdf))
+							.addGap(18)
 							.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 396, GroupLayout.PREFERRED_SIZE))
 						.addGroup(gl_contentPane.createSequentialGroup()
 							.addGap(183)
 							.addComponent(lblNewLabel)))
-					.addContainerGap(40, Short.MAX_VALUE))
+					.addContainerGap(25, Short.MAX_VALUE))
 		);
 		contentPane.setLayout(gl_contentPane);
 	}
-	
+
 	public void setRequirementsTraceabilityMatrix(RequirementsTraceabilityMatrixByImpact requirementsTraceabilityMatrixByImpact){
 		this.requirementsTraceabilityMatrixByImpact = requirementsTraceabilityMatrixByImpact;
 	}
@@ -132,4 +150,23 @@ public class TraceabilityMatrixByImpactViewHandler extends JFrame {
 
 		
 	}
+
+
+	//todo move to gui utils?
+	private File chooseFile(String filename) {
+//		final String ext = extension;
+		JFileChooser fileChooser = new JFileChooser();
+		fileChooser.setDialogTitle("Specify a file to save");
+		fileChooser.setSelectedFile(new File(filename));
+
+		int userSelection = fileChooser.showSaveDialog(this);
+
+		if (userSelection == JFileChooser.APPROVE_OPTION) {
+			return fileChooser.getSelectedFile();
+		}
+		return null;
+	}
+
+
+
 }
