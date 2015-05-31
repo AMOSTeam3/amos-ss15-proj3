7f64a05fe7a71a7a154e01e92fab6ab6c2fbbb8f
Dmitry Gorelenkov <dmitry.gorelenkov@gmx.net>
2015-05-31 21:16:34 +0200

    refactor to make more useful Req-50

--- src/main/java/de/fau/osr/core/db/HibernateUtil.java
+++ src/main/java/de/fau/osr/core/db/HibernateUtil.java
@@ -1,11 +1,15 @@
 package de.fau.osr.core.db;
 
+import de.fau.osr.core.db.domain.Requirement;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.HashSet;
+import java.util.Set;
 
 
 /**
@@ -18,17 +22,32 @@ public class HibernateUtil {
     static Logger logger = LoggerFactory.getLogger(HibernateUtil.class);
 
     private static SessionFactory sessionFactory ;
-   
     static {
         logger.debug("Static call : getsession factory() start::");
 
-        Configuration configuration = new Configuration().configure();
+        //generate configuration
+        Configuration configuration = getConfiguration();
+
         StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties());
         sessionFactory = configuration.buildSessionFactory(builder.build());
         
         logger.debug("Static call : getsession factory() end::");
     }
-  
+
+    /**
+     * generates configuration from hibernate.cfg.xml and adds all persistent classes to it
+     * @return generated configuration
+     */
+    public static Configuration getConfiguration() {
+        Configuration configuration = new Configuration().configure();
+        //add persistent classes
+        for(Class<?> clazz : getPersistentClasses())
+        {
+            configuration.addAnnotatedClass(clazz);
+        }
+        return configuration;
+    }
+
     public static SessionFactory getSessionFactory() {
        return sessionFactory;
    }
@@ -44,6 +63,24 @@ public class HibernateUtil {
     public void closeSession(Session session) {
         session.clear();
         session.close();
+    }
+
+    /**
+     * generates persistent classes set, for now hardcoded.
+     * todo use reflection, or spring
+     * @see <a href="http://stackoverflow.com/questions/1413190/hibernate-mapping-package">http://stackoverflow.com/questions/1413190/hibernate-mapping-package</a>
+     * @return set of classes are persistent
+     */
+    static public Set<Class<?>> getPersistentClasses(){
+        //reflection way
+//        Reflections reflections = new Reflections("your_package");
+//        Set<Class<?>> classes = reflections.getTypesAnnotatedWith(javax.persistence.Entity.class);
+
+        //hardcoded way for now
+        Set<Class<?>> classes = new HashSet<>();
+        classes.add(Requirement.class);
+
+        return classes;
     }
 
 }
