2c0c0811dc2bbfe63bacb74788e384e167d1c821
Dmitry Gorelenkov <dmitry.gorelenkov@gmx.net>
2015-06-02 16:32:26 +0200

    bugfix: close connections at the end of app.

--- src/main/java/de/fau/osr/gui/Application.java
+++ src/main/java/de/fau/osr/gui/Application.java
@@ -1,10 +1,13 @@
 package de.fau.osr.gui;
 
 
+import de.fau.osr.core.db.HibernateUtil;
+
 public class Application {
     static GuiController guiController;
 
     public static void main(String[] args) {
         guiController = new GuiController();
+        HibernateUtil.shutdown();
     }
 }
