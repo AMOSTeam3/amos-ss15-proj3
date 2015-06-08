9cae9822224ec10b39727f77ca07afb0845b4aae
gayathery <gayatherys@gmail.com>
2015-06-08 21:46:38 +0200

    Req-023 Default Requirement pattern for the eclipse plugin is implemented to be taken from properties file

--- amos-ss15-proj3-eclipse-plugin/src/osr/core/RegistrySettings.java
+++ amos-ss15-proj3-eclipse-plugin/src/osr/core/RegistrySettings.java
@@ -1,8 +1,10 @@
 package osr.core;
 
+import de.fau.osr.util.AppProperties;
+
 public class RegistrySettings {
 
 	public static String repoURL;
 	//TODO:Need to get it from User at the startup.
-	public static String requirementPattern="[Rr]eq-0*(\\d+)";
+	public static String requirementPattern=AppProperties.GetValue("RequirementPattern");
 }
