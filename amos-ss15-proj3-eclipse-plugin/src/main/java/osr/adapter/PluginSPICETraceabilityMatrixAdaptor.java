807942a08298be5485f9fc05f43b4d0cb784bd48
gayathery <gayatherys@gmail.com>
2015-06-20 00:46:26 +0200

    Issue-88 Enhancement: Reuse functionality for Traceability Matrix progressbar.
    Fixes #88

--- amos-ss15-proj3-eclipse-plugin/src/main/java/osr/adapter/PluginSPICETraceabilityMatrixAdaptor.java
+++ amos-ss15-proj3-eclipse-plugin/src/main/java/osr/adapter/PluginSPICETraceabilityMatrixAdaptor.java
@@ -1,74 +1,35 @@
 package osr.adapter;
 
 import de.fau.osr.bl.RequirementsTraceabilityMatrixByImpact;
-import de.fau.osr.gui.View.TraceabilityMatrixByImpactViewHandler;
-import de.fau.osr.gui.util.SpiceTraceabilityProgressBar;
-
-import javax.swing.*;
-import java.awt.event.WindowEvent;
+import de.fau.osr.gui.View.TracabilityMatrix_View;
 
 /**
- * @author Gayathery
- * This class uses the backed Traceability Matrix functionality, the calls are adapted in this class.
+ * @author Gayathery This class uses the backed Traceability Matrix
+ *         functionality, the calls are adapted in this class.
  */
 public class PluginSPICETraceabilityMatrixAdaptor {
-    
-    public void loadTraceabilityMatrixByImpact(){
-        PluginSPICETrackerAdaptor.getInstance(); 
+
+    TracabilityMatrix_View matrixView = new TracabilityMatrix_View();
+
+    public void loadTraceabilityMatrixByImpact() {
+        PluginSPICETrackerAdaptor.getInstance();
         class TraceabilityMatrixViewerThread implements Runnable {
 
             @Override
             public void run() {
-                showTraceabilityMatrixByImpactProgressBar();
-                TraceabilityMatrixByImpactViewHandler trMatrixByImpact = new TraceabilityMatrixByImpactViewHandler();
-                trMatrixByImpact.setRequirementsTraceabilityMatrix(PluginSPICETrackerAdaptor.getTracker().generateRequirementsTraceabilityByImpact());
-                trMatrixByImpact.initTable();
-                trMatrixByImpact.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-                trMatrixByImpact.setVisible(true);
-                
-            }
-
-        }
-        Thread tr = new Thread(new TraceabilityMatrixViewerThread());
-        tr.start(); 
-       
-    }
-    
-    public void showTraceabilityMatrixByImpactProgressBar(){
-
-        final SpiceTraceabilityProgressBar progressBar = new SpiceTraceabilityProgressBar();
-        progressBar.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        progressBar.setProgressBarContent("Generating Traceability Matrix (by Impact)");
-        progressBar.setVisible(true);
-
-        class showTraceabilityMatrixByImpactProgressBarThread implements Runnable{
-
-            @Override
-            public void run() {
-                Boolean completion = false;
-                while(RequirementsTraceabilityMatrixByImpact.processProgress<=100){
-                    progressBar.setProgressBarValue(RequirementsTraceabilityMatrixByImpact.processProgress);
-                    try {
-                        Thread.sleep(100);
-
-                        if(completion)
-                            break;
-                        if(RequirementsTraceabilityMatrixByImpact.processProgress == 100){
-                            completion = true;
-                        }
-                    } catch (InterruptedException e) {
-                        // TODO Auto-generated catch block
-                        e.printStackTrace();
-                    }
-
-                }
-                progressBar.dispatchEvent(new WindowEvent(progressBar, WindowEvent.WINDOW_CLOSING));
+                matrixView.showTraceabilityMatrixByImpactProgressBar();
+                RequirementsTraceabilityMatrixByImpact requirementsTraceabilityMatrixByImpact = PluginSPICETrackerAdaptor
+                        .getTracker()
+                        .generateRequirementsTraceabilityByImpact();
+                matrixView
+                        .showTraceabilityMatrixByImpact(requirementsTraceabilityMatrixByImpact);
 
             }
 
         }
-        Thread tr = new Thread(new showTraceabilityMatrixByImpactProgressBarThread());
-        tr.start();
+        Thread thread = new Thread(new TraceabilityMatrixViewerThread());
+        thread.start();
+
     }
 
 }
