8e17368802300e0b62c05e96d39bfc237c6a6e3d
Tobias Polzer <tobias.polzer@fau.de>
2015-07-10 16:27:25 +0200

    Fix product name in licence header
    Changed from Req-Tracker to ReqTracker

--- amos-ss15-proj3-gui/src/main/java/de/fau/osr/gui/Components/PathDEsJTree.java
+++ amos-ss15-proj3-gui/src/main/java/de/fau/osr/gui/Components/PathDEsJTree.java
@@ -1,22 +1,22 @@
 /*
- * This file is part of Req-Tracker.
+ * This file is part of ReqTracker.
  *
  * Copyright (C) 2015 Taleh Didover, Florian Gerdes, Dmitry Gorelenkov,
  *     Rajab Hassan Kaoneka, Katsiaryna Krauchanka, Tobias Polzer,
  *     Gayathery Sathya, Lukas Tajak
  *
- * Req-Tracker is free software: you can redistribute it and/or modify
+ * ReqTracker is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Req-Tracker is distributed in the hope that it will be useful,
+ * ReqTracker is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
  *
  * You should have received a copy of the GNU Affero General Public License
- * along with Req-Tracker.  If not, see <http://www.gnu.org/licenses/>.
+ * along with ReqTracker.  If not, see <http://www.gnu.org/licenses/>.
  */
 package de.fau.osr.gui.Components;
 
